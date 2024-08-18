e2a228fe571e4239d61c593cf312e1184d913adb
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-08-18 23:12:52 +0200

    feat: init commit, github oauth

---
+++ src/app/components/NavMenu.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const AuthButton = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session.user?.name} <br />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign In</button>
+    </>
+  );
+
+  return <div>test</div>;
+};
+
+const NavMenu = () => {
+  return (
+    <div>
+      <AuthButton />
+    </div>
+  );
+};
+
+export default NavMenu;
